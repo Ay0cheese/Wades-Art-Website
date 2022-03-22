4095ec332eec24f0e19cc5f92b07081bc1e77ab7
Ay0cheese <94671327+Ay0cheese@users.noreply.github.com>
2022-03-22 01:07:19 -0700

    Add files via upload

--- validation.js
+++ validation.js
@@ -1,9 +1,15 @@
+
+// TODO: //
+//1: put a drop down in the product type area, and stype the <p> like the others//
+
+
 const inputs = document.querySelectorAll('input');
 
 const patterns = {
-//fName:/^[a-z]$/gix,
-    phone:/^\d{10}$/gi,
-
+    fName:/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u,  
+    email:/^([a-z\d\.-_]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2-8})?$/,
+    phone:/^\d{3}(-)?\d{3}(-)?\d{4}$/gi,
+    description:/^[ A-Za-z0-9_@./!#&$+-]*$/ 
 };
 
 // validation function//
